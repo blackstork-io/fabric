b049864e2d684d47cc4007f1bf6ff03f33a4338a
Andrew Morozko <github-msgs-web@bitnotes.xyz>
2024-02-08 21:42:46 +0400

    Removed descriptions from main binary

--- cmd/root.go
+++ cmd/root.go
@@ -71,14 +71,7 @@ func validateDir(what, dir string) error {
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "fabric",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "fabric",
 	Version: version,
 
 	PersistentPreRunE: func(_ *cobra.Command, _ []string) (err error) {
