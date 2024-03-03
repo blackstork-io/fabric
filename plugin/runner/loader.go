65cf2d52f8e1006ca2be9be4306e38972b3e29ba
dobarx <dbarzdys@proton.me>
2024-03-03 20:35:16 +0200

    plugin/runner: use plugin resolver from other package

--- plugin/runner/loader.go
+++ plugin/runner/loader.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/hashicorp/hcl/v2"
@@ -26,18 +27,18 @@ type loadedContentProvider struct {
 }
 
 type loader struct {
-	resolver   *resolver
-	versionMap VersionMap
-	builtin    []*plugin.Schema
+	logger     *slog.Logger
+	binaryMap  map[string]string
+	builtin    *plugin.Schema
 	pluginMap  map[string]loadedPlugin
 	dataMap    map[string]loadedDataSource
 	contentMap map[string]loadedContentProvider
 }
 
-func makeLoader(mirrorDir string, builtin []*plugin.Schema, pluginMap VersionMap) *loader {
+func makeLoader(binaryMap map[string]string, builtin *plugin.Schema, logger *slog.Logger) *loader {
 	return &loader{
-		resolver:   makeResolver(mirrorDir),
-		versionMap: pluginMap,
+		logger:     logger,
+		binaryMap:  binaryMap,
 		builtin:    builtin,
 		pluginMap:  make(map[string]loadedPlugin),
 		dataMap:    make(map[string]loadedDataSource),
@@ -50,14 +51,12 @@ func nopCloser() error {
 }
 
 func (l *loader) loadAll() hcl.Diagnostics {
-	for _, p := range l.builtin {
-		if diags := l.registerPlugin(p, nopCloser); diags.HasErrors() {
-			diags = append(diags, l.closeAll()...)
-			return diags
-		}
+	if diags := l.registerPlugin(l.builtin, nopCloser); diags.HasErrors() {
+		diags = append(diags, l.closeAll()...)
+		return diags
 	}
-	for name, version := range l.versionMap {
-		if diags := l.loadBinary(name, version); diags.HasErrors() {
+	for name, binaryPath := range l.binaryMap {
+		if diags := l.loadBinary(name, binaryPath); diags.HasErrors() {
 			diags = append(diags, l.closeAll()...)
 			return diags
 		}
@@ -79,92 +78,95 @@ func (l *loader) closeAll() hcl.Diagnostics {
 	return diags
 }
 
-func (l *loader) registerDataSource(name string, p *plugin.Schema, ds *plugin.DataSource) hcl.Diagnostics {
+func (l *loader) registerDataSource(name string, schema *plugin.Schema, ds *plugin.DataSource) hcl.Diagnostics {
 	if found, has := l.dataMap[name]; has {
 		return hcl.Diagnostics{{
 			Severity: hcl.DiagError,
 			Summary:  "Duplicate data source",
-			Detail:   fmt.Sprintf("Data source %s provided by plugin %s@%s and %s@%s", name, p.Name, p.Version, found.plugin.Name, found.plugin.Version),
+			Detail:   fmt.Sprintf("Data source %s provided by plugin %s@%s and %s@%s", name, schema.Name, schema.Version, found.plugin.Name, found.plugin.Version),
 		}}
 	}
-	l.dataMap[name] = loadedDataSource{p, ds}
+	l.dataMap[name] = loadedDataSource{
+		plugin:     schema,
+		DataSource: ds,
+	}
 	return nil
 }
 
-func (l *loader) registerContentProvider(name string, p *plugin.Schema, cp *plugin.ContentProvider) hcl.Diagnostics {
+func (l *loader) registerContentProvider(name string, schema *plugin.Schema, cp *plugin.ContentProvider) hcl.Diagnostics {
 	if found, has := l.contentMap[name]; has {
 		return hcl.Diagnostics{{
 			Severity: hcl.DiagError,
 			Summary:  "Duplicate content provider",
-			Detail:   fmt.Sprintf("Content provider %s provided by plugin %s@%s and %s@%s", name, p.Name, p.Version, found.plugin.Name, found.plugin.Version),
+			Detail:   fmt.Sprintf("Content provider %s provided by plugin %s@%s and %s@%s", name, schema.Name, schema.Version, found.plugin.Name, found.plugin.Version),
 		}}
 	}
-	l.contentMap[name] = loadedContentProvider{p, cp}
+	l.contentMap[name] = loadedContentProvider{
+		plugin:          schema,
+		ContentProvider: cp,
+	}
 	return nil
 }
 
-func (l *loader) registerPlugin(p *plugin.Schema, closefn func() error) hcl.Diagnostics {
-	if diags := p.Validate(); diags.HasErrors() {
+func (l *loader) registerPlugin(schema *plugin.Schema, closefn func() error) hcl.Diagnostics {
+	if diags := schema.Validate(); diags.HasErrors() {
 		return diags
 	}
-	if found, has := l.pluginMap[p.Name]; has {
+	if found, has := l.pluginMap[schema.Name]; has {
 		diags := hcl.Diagnostics{{
 			Severity: hcl.DiagError,
-			Summary:  "Plugin conflict",
-			Detail:   fmt.Sprintf("Plugin %s@%s and %s@%s have the same name", p.Name, p.Version, found.Name, found.Version),
+			Summary:  fmt.Sprintf("Plugin %s conflict", schema.Name),
+			Detail:   fmt.Sprintf("%s@%s and %s@%s have the same schema name", schema.Name, schema.Version, found.Name, found.Version),
 		}}
 		err := found.closefn()
 		if err != nil {
 			diags = append(diags, &hcl.Diagnostic{
 				Severity: hcl.DiagError,
-				Summary:  "Failed to close plugin",
-				Detail:   fmt.Sprintf("Failed to close plugin %s@%s: %v", found.Name, found.Version, err),
+				Summary:  fmt.Sprintf("Failed to close plugin %s@%s", found.Name, found.Version),
+				Detail:   err.Error(),
 			})
 		}
 		return diags
 	}
 	plugin := loadedPlugin{
 		closefn: closefn,
-		Schema:  p,
+		Schema:  schema,
 	}
-	l.pluginMap[p.Name] = plugin
-	for name, source := range p.DataSources {
-		if diags := l.registerDataSource(name, p, source); diags.HasErrors() {
+	l.pluginMap[schema.Name] = plugin
+	for name, source := range schema.DataSources {
+		if diags := l.registerDataSource(name, schema, source); diags.HasErrors() {
 			return diags
 		}
 	}
-	for name, provider := range p.ContentProviders {
-		if diags := l.registerContentProvider(name, p, provider); diags.HasErrors() {
+	for name, provider := range schema.ContentProviders {
+		if diags := l.registerContentProvider(name, schema, provider); diags.HasErrors() {
 			return diags
 		}
 	}
 	return nil
 }
 
-func (l *loader) loadBinary(name, version string) hcl.Diagnostics {
-	loc, diags := l.resolver.resolve(name, version)
-	if diags.HasErrors() {
-		return diags
-	}
-	if info, err := os.Stat(loc); os.IsNotExist(err) {
+func (l *loader) loadBinary(name, binaryPath string) hcl.Diagnostics {
+	if info, err := os.Stat(binaryPath); os.IsNotExist(err) {
 		return hcl.Diagnostics{{
 			Severity: hcl.DiagError,
-			Summary:  "Plugin not found",
-			Detail:   fmt.Sprintf("Plugin %s@%s not found at: %s", name, version, loc),
+			Summary:  fmt.Sprintf("Plugin %s binary not found", name),
+			Detail:   fmt.Sprintf("Executable not found at: %s", binaryPath),
 		}}
 	} else if info.IsDir() {
 		return hcl.Diagnostics{{
 			Severity: hcl.DiagError,
-			Summary:  "Plugin is a directory",
-			Detail:   fmt.Sprintf("Plugin %s@%s is a directory at: %s", name, version, loc),
+			Summary:  fmt.Sprintf("Plugin %s binary path is a directory", name),
+			Detail:   fmt.Sprintf("Path %s is a directory", binaryPath),
 		}}
 	}
-	p, close, err := pluginapiv1.NewClient(loc)
+	l.logger.Info("Loading plugin", "name", name, "path", binaryPath)
+	p, close, err := pluginapiv1.NewClient(name, binaryPath, l.logger)
 	if err != nil {
 		return hcl.Diagnostics{{
 			Severity: hcl.DiagError,
-			Summary:  "Failed to create plugin client",
-			Detail:   fmt.Sprintf("Failed to create plugin client for %s@%s: %v", name, version, err),
+			Summary:  fmt.Sprintf("Failed to load plugin %s", name),
+			Detail:   err.Error(),
 		}}
 	}
 	return l.registerPlugin(p, close)
--- plugin/runner/runner.go
+++ plugin/runner/runner.go
@@ -2,91 +2,25 @@ package runner
 
 import (
 	"fmt"
-	"strings"
-	"unicode"
+	"log/slog"
 
-	"github.com/Masterminds/semver/v3"
 	"github.com/hashicorp/hcl/v2"
 
 	"github.com/blackstork-io/fabric/plugin"
 )
 
-type VersionMap map[string]string
-
 type Runner struct {
-	cacheDir   string
 	pluginMap  map[string]loadedPlugin
 	dataMap    map[string]loadedDataSource
 	contentMap map[string]loadedContentProvider
 }
 
-func validatePluginName(name string) hcl.Diagnostics {
-	parts := strings.Split(name, "/")
-	if len(parts) != 2 {
-		return hcl.Diagnostics{{
-			Severity: hcl.DiagError,
-			Summary:  "Invalid plugin name",
-			Detail:   fmt.Sprintf("plugin name '%s' is not in the form '<namespace>/<name>'", name),
-		}}
-	}
-	for _, r := range parts[0] {
-		if !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '-' {
-			return hcl.Diagnostics{{
-				Severity: hcl.DiagError,
-				Summary:  "Invalid plugin name",
-				Detail:   fmt.Sprintf("plugin name '%s' contains invalid character: '%c'", name, r),
-			}}
-		}
-	}
-	for _, r := range parts[1] {
-		if !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '-' {
-			return hcl.Diagnostics{{
-				Severity: hcl.DiagError,
-				Summary:  "Invalid plugin name",
-				Detail:   fmt.Sprintf("plugin name '%s' contains invalid character: '%c'", name, r),
-			}}
-		}
-	}
-	return nil
-}
-
-func validatePluginVersionMap(versionMap VersionMap) (diags hcl.Diagnostics) {
-	for name, version := range versionMap {
-		diags = validatePluginName(name).Extend(diags)
-		if version == "" {
-			diags = diags.Append(&hcl.Diagnostic{
-				Severity: hcl.DiagError,
-				Summary:  "Missing plugin version",
-				Detail:   fmt.Sprintf("Missing plugin version for '%s'", name),
-			})
-			continue
-		}
-		_, err := semver.NewConstraint(version)
-		if err != nil {
-			diags = diags.Append(&hcl.Diagnostic{
-				Severity: hcl.DiagError,
-				Summary:  "Invalid plugin version",
-				Detail:   fmt.Sprintf("Invalid version constraint for '%s': %s", name, err),
-			})
-		}
-	}
-	return diags
-}
-
-func Load(o ...Option) (*Runner, hcl.Diagnostics) {
-	opts := defaultOptions
-	for _, opt := range o {
-		opt(&opts)
-	}
-	if diags := validatePluginVersionMap(opts.versionMap); diags.HasErrors() {
-		return nil, diags
-	}
-	loader := makeLoader(opts.pluginDir, opts.builtin, opts.versionMap)
+func Load(binaryMap map[string]string, builtin *plugin.Schema, logger *slog.Logger) (*Runner, hcl.Diagnostics) {
+	loader := makeLoader(binaryMap, builtin, logger)
 	if diags := loader.loadAll(); diags.HasErrors() {
 		return nil, diags
 	}
 	return &Runner{
-		cacheDir:   opts.pluginDir,
 		pluginMap:  loader.pluginMap,
 		dataMap:    loader.dataMap,
 		contentMap: loader.contentMap,
@@ -98,8 +32,8 @@ func (m *Runner) DataSource(name string) (*plugin.DataSource, hcl.Diagnostics) {
 	if !has {
 		return nil, hcl.Diagnostics{{
 			Severity: hcl.DiagError,
-			Summary:  "Data source not found",
-			Detail:   fmt.Sprintf("data source '%s' not found in any plugin", name),
+			Summary:  fmt.Sprintf("Missing data source '%s'", name),
+			Detail:   fmt.Sprintf("'%s' not found in any plugin", name),
 		}}
 	}
 	return source.DataSource, nil
@@ -110,8 +44,8 @@ func (m *Runner) ContentProvider(name string) (*plugin.ContentProvider, hcl.Diag
 	if !has {
 		return nil, hcl.Diagnostics{{
 			Severity: hcl.DiagError,
-			Summary:  "Content provider not found",
-			Detail:   fmt.Sprintf("content provider '%s' not found in any plugin", name),
+			Summary:  fmt.Sprintf("Missing content provider '%s'", name),
+			Detail:   fmt.Sprintf("'%s' not found in any plugin", name),
 		}}
 	}
 	return provider.ContentProvider, nil
@@ -123,8 +57,8 @@ func (m *Runner) Close() hcl.Diagnostics {
 		if err := p.closefn(); err != nil {
 			diags = append(diags, &hcl.Diagnostic{
 				Severity: hcl.DiagWarning,
-				Summary:  "Failed to close plugin",
-				Detail:   fmt.Sprintf("failed to close plugin %s@%s: %v", p.Name, p.Version, err),
+				Summary:  fmt.Sprintf("Failed to close plugin '%s'", p.Name),
+				Detail:   err.Error(),
 			})
 		}
 	}
