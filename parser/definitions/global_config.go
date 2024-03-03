1d819e3ccaad6c13d53ebe81119fd24762115b4e
dobarx <dbarzdys@proton.me>
2024-03-03 20:36:21 +0200

    parser: simplify global config parsing

--- parser/definitions/global_config.go
+++ parser/definitions/global_config.go
@@ -1,105 +1,31 @@
 package definitions
 
-import (
-	"fmt"
-
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hcldec"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/convert"
-
-	"github.com/blackstork-io/fabric/pkg/diagnostics"
-)
-
-var globalConfigSpec = &hcldec.ObjectSpec{
-	"cache_dir": &hcldec.AttrSpec{
-		Name:     "cache_dir",
-		Type:     cty.String,
-		Required: false,
-	},
-	"plugin_registry": &hcldec.BlockSpec{
-		TypeName: "plugin_registry",
-		Nested: hcldec.ObjectSpec{
-			"mirror_dir": &hcldec.AttrSpec{
-				Name:     "mirror_dir",
-				Type:     cty.String,
-				Required: false,
-			},
-		},
-	},
-	"plugin_versions": &hcldec.AttrSpec{
-		Name:     "plugin_versions",
-		Type:     cty.Map(cty.String),
-		Required: false,
-	},
-}
-
 type GlobalConfig struct {
-	block          *hclsyntax.Block
-	CacheDir       string
-	PluginRegistry *PluginRegistry
-	PluginVersions map[string]string
+	CacheDir       string            `hcl:"cache_dir,optional"`
+	PluginRegistry *PluginRegistry   `hcl:"plugin_registry,block"`
+	PluginVersions map[string]string `hcl:"plugin_versions,optional"`
 }
 
 type PluginRegistry struct {
-	MirrorDir string
+	BaseURL   string `hcl:"base_url,optional"`
+	MirrorDir string `hcl:"mirror_dir,optional"`
 }
 
-func DefineGlobalConfig(block *hclsyntax.Block) (cfg *GlobalConfig, diags diagnostics.Diag) {
-	if len(block.Labels) > 0 {
-		return nil, diagnostics.Diag{{
-			Severity: hcl.DiagError,
-			Summary:  "Invalid global config",
-			Detail:   "Global config should not have labels",
-		}}
+func (g *GlobalConfig) Merge(other *GlobalConfig) {
+	if other.CacheDir != "" {
+		g.CacheDir = other.CacheDir
 	}
-	value, hclDiags := hcldec.Decode(block.Body, globalConfigSpec, nil)
-	if diags.ExtendHcl(hclDiags) {
-		return
-	}
-	typ := hcldec.ImpliedType(globalConfigSpec)
-	errs := value.Type().TestConformance(typ)
-	if len(errs) > 0 {
-		var err error
-		value, err = convert.Convert(value, typ)
-		if err != nil {
-			diags.AppendErr(err, "Error while serializing global config")
-			return
-		}
-	}
-	cfg = &GlobalConfig{
-		block:          block,
-		CacheDir:       "./.fabric",
-		PluginVersions: make(map[string]string),
-	}
-	cacheDir := value.GetAttr("cache_dir")
-	if !cacheDir.IsNull() && cacheDir.AsString() != "" {
-		cfg.CacheDir = cacheDir.AsString()
-	}
-	pluginRegistry := value.GetAttr("plugin_registry")
-	if !pluginRegistry.IsNull() {
-		mirrorDir := pluginRegistry.GetAttr("mirror_dir")
-		if !mirrorDir.IsNull() || mirrorDir.AsString() != "" {
-			cfg.PluginRegistry = &PluginRegistry{
-				MirrorDir: mirrorDir.AsString(),
+	if other.PluginRegistry != nil {
+		if g.PluginRegistry == nil {
+			g.PluginRegistry = other.PluginRegistry
+		} else {
+			if other.PluginRegistry.BaseURL != "" {
+				g.PluginRegistry.BaseURL = other.PluginRegistry.BaseURL
+			}
+			if other.PluginRegistry.MirrorDir != "" {
+				g.PluginRegistry.MirrorDir = other.PluginRegistry.MirrorDir
 			}
 		}
 	}
-	pluginVersions := value.GetAttr("plugin_versions")
-	if !pluginVersions.IsNull() {
-		versionMap := pluginVersions.AsValueMap()
-		for k, v := range versionMap {
-			if v.Type() != cty.String {
-				diags.Append(&hcl.Diagnostic{
-					Severity: hcl.DiagError,
-					Summary:  "Invalid plugin version",
-					Detail:   fmt.Sprintf("Version of plugin '%s' should be a string", k),
-				})
-				continue
-			}
-			cfg.PluginVersions[k] = v.AsString()
-		}
-	}
-	return cfg, nil
+	g.PluginVersions = other.PluginVersions
 }
