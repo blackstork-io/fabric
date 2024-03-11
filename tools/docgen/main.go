e0f9256436c1abdc93a10c6e6d37d53ca2524e90
traut <sergey@polzunov.com>
2024-03-11 12:23:30 +0100

    Enable generation of content provider / data sources / plugin docs separately

--- tools/docgen/main.go
+++ tools/docgen/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json"
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,6 +19,7 @@ import (
 	"github.com/blackstork-io/fabric/internal/graphql"
 	"github.com/blackstork-io/fabric/internal/hackerone"
 	"github.com/blackstork-io/fabric/internal/openai"
+	"github.com/blackstork-io/fabric/internal/opencti"
 	"github.com/blackstork-io/fabric/internal/postgresql"
 	"github.com/blackstork-io/fabric/internal/splunk"
 	"github.com/blackstork-io/fabric/internal/sqlite"
@@ -31,10 +34,166 @@ var (
 	outputDir string
 )
 
-//go:embed markdown.gotempl
-var markdownTempl string
+//go:embed content-provider.md.gotempl
+var contentProviderTemplValue string
 
-var templ *template.Template
+var contentProviderTempl *template.Template
+
+//go:embed data-source.md.gotempl
+var dataSourceTemplValue string
+
+var dataSourceTempl *template.Template
+
+//go:embed plugin.md.gotempl
+var pluginTemplValue string
+
+var pluginTempl *template.Template
+
+type PluginResourceMeta struct {
+	Name         string   `json:"name"`
+	Type         string   `json:"type"`
+	ConfigParams []string `json:"config_params,omitempty"`
+	Arguments    []string `json:"arguments,omitempty"`
+}
+
+func generateDataSourceDocs(log *slog.Logger, p *plugin.Schema, outputDir string) {
+	log.Info("Found data sources inside the plugin", "count", len(p.DataSources))
+
+	dataSourcesDir := filepath.Join(outputDir, "data-sources")
+
+	// Create a directory for plugin's data sources if it doesn't exist
+	err := os.MkdirAll(dataSourcesDir, 0766)
+	if err != nil {
+		log.Error("Can't create a directory", "path", dataSourcesDir)
+		panic(err)
+	}
+
+	for name, dataSource := range p.DataSources {
+		log.Info("Found a data source", "name", name)
+		docFilename := fmt.Sprintf("%s.md", name)
+		docPath := filepath.Join(dataSourcesDir, docFilename)
+		err := renderDataSourceDoc(p, name, dataSource, docPath)
+		if err != nil {
+			log.Error("Error while rendering a data source doc", "plugin", p.Name, "dataSource", name)
+			panic(err)
+		}
+	}
+}
+
+func generateContentProviderDocs(log *slog.Logger, p *plugin.Schema, outputDir string) {
+	log.Info("Found content providers inside the plugin", "count", len(p.ContentProviders))
+
+	contentProvidersDir := filepath.Join(outputDir, "content-providers")
+
+	// Create a directory for plugin's content providers if it doesn't exist
+	err := os.MkdirAll(contentProvidersDir, 0766)
+	if err != nil {
+		log.Error("Can't create a directory", "path", contentProvidersDir)
+		panic(err)
+	}
+
+	for name, contentProvider := range p.ContentProviders {
+		log.Info("Found a content provider", "name", name)
+
+		docFilename := fmt.Sprintf("%s.md", name)
+		docPath := filepath.Join(contentProvidersDir, docFilename)
+		err := renderContentProviderDoc(p, name, contentProvider, docPath)
+		if err != nil {
+			log.Error("Error while rendering a content provider doc", "plugin", p.Name, "contentProvider", name)
+			panic(err)
+		}
+	}
+}
+
+func marshalDataSource(name string, ds *plugin.DataSource) PluginResourceMeta {
+
+	var configParams []string
+	configSpec, ok := ds.Config.(hcldec.ObjectSpec)
+	if ok && configSpec != nil {
+		for k := range configSpec {
+			configParams = append(configParams, k)
+		}
+	}
+
+	var arguments []string
+	argsSpec, ok := ds.Args.(hcldec.ObjectSpec)
+	if ok && argsSpec != nil {
+		for k := range argsSpec {
+			arguments = append(arguments, k)
+		}
+	}
+	return PluginResourceMeta{
+		Name:         name,
+		Type:         "data-source",
+		ConfigParams: configParams,
+		Arguments:    arguments,
+	}
+}
+
+func marshalContentProvider(name string, p *plugin.ContentProvider) PluginResourceMeta {
+
+	var configParams []string
+	configSpec, ok := p.Config.(hcldec.ObjectSpec)
+	if ok && configSpec != nil {
+		for k := range configSpec {
+			configParams = append(configParams, k)
+		}
+	}
+
+	var arguments []string
+	argsSpec, ok := p.Args.(hcldec.ObjectSpec)
+	if ok && argsSpec != nil {
+		for k := range argsSpec {
+			arguments = append(arguments, k)
+		}
+	}
+	return PluginResourceMeta{
+		Name:         name,
+		Type:         "content-provider",
+		ConfigParams: configParams,
+		Arguments:    arguments,
+	}
+}
+
+func generateMetadataFile(plugins []*plugin.Schema, outputDir string) {
+	pluginDetails := make([]any, len(plugins))
+
+	for i, p := range plugins {
+
+		var resources []PluginResourceMeta
+
+		for name, dataSource := range p.DataSources {
+			resources = append(resources, marshalDataSource(name, dataSource))
+		}
+		for name, contentProvider := range p.ContentProviders {
+			resources = append(resources, marshalContentProvider(name, contentProvider))
+		}
+
+		pluginDetails[i] = map[string]any{
+			"name":      p.Name,
+			"version":   p.Version,
+			"shortname": shortname(p.Name),
+			"resources": resources,
+		}
+	}
+
+	jsonData, err := json.MarshalIndent(pluginDetails, "", "  ")
+	if err != nil {
+		slog.Error("Failed to marshal the plugin details into JSON")
+		return
+	}
+
+	pluginDetailsPath := filepath.Join(outputDir, "plugins.json")
+	file, err := os.Create(pluginDetailsPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	file.Write(jsonData)
+
+	slog.Info("Plugin details file generated", "path", pluginDetailsPath)
+}
 
 func main() {
 	// parse flags
@@ -42,10 +201,6 @@ func main() {
 	flags.StringVar(&version, "version", "v0.0.0-dev", "version of the build")
 	flags.StringVar(&outputDir, "output", "./dist/docs", "output directory")
 	flags.Parse(os.Args[1:])
-	// ensure output directory exists
-	if err := os.MkdirAll(outputDir, 0o755); err != nil {
-		panic(err)
-	}
 	// load all plugins
 	plugins := []*plugin.Schema{
 		builtin.Plugin(version),
@@ -53,6 +208,7 @@ func main() {
 		github.Plugin(version, nil),
 		graphql.Plugin(version),
 		openai.Plugin(version, nil),
+		opencti.Plugin(version),
 		postgresql.Plugin(version),
 		sqlite.Plugin(version),
 		terraform.Plugin(version),
@@ -63,21 +219,81 @@ func main() {
 	}
 	// generate markdown for each plugin
 	for _, p := range plugins {
-		fp := filepath.Join(outputDir, fmt.Sprintf("%s.md", shortname(p.Name)))
-		fmt.Printf("Generating '%s': '%s'\n", p.Name, fp)
-		if err := generate(p, fp); err != nil {
+
+		log := slog.With("plugin", p.Name)
+
+		pluginShortname := shortname(p.Name)
+
+		// Use a shortname as a plugin directory name
+		pluginOutputDir := filepath.Join(outputDir, pluginShortname)
+
+		// Create a plugin directory if it doesn't exist
+		err := os.MkdirAll(pluginOutputDir, 0766)
+		if err != nil {
+			log.Error("Can't create a plugin directory", "path", pluginOutputDir)
 			panic(err)
 		}
+
+		pluginDocPath := filepath.Join(pluginOutputDir, "_index.md")
+		err = renderPluginDoc(p, pluginDocPath)
+		if err != nil {
+			log.Error("Error while rendering a plugin doc", "plugin", p.Name)
+			panic(err)
+		}
+
+		log.Info("Plugin doc rendered", "path", pluginDocPath)
+
+		if len(p.DataSources) != 0 {
+			generateDataSourceDocs(log, p, pluginOutputDir)
+		}
+
+		if len(p.ContentProviders) != 0 {
+			generateContentProviderDocs(log, p, pluginOutputDir)
+		}
 	}
+	generateMetadataFile(plugins, outputDir)
 }
 
-func generate(schema *plugin.Schema, fp string) error {
+func renderPluginDoc(pluginSchema *plugin.Schema, fp string) error {
 	f, err := os.Create(fp)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	return templ.Execute(f, schema)
+
+	return pluginTempl.Execute(f, pluginSchema)
+}
+
+func renderContentProviderDoc(pluginSchema *plugin.Schema, contentProviderName string, contentProvider *plugin.ContentProvider, fp string) error {
+	f, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var templContext = map[string]any{
+		"plugin":           pluginSchema,
+		"plugin_shortname": shortname(pluginSchema.Name),
+		"name":             contentProviderName,
+		"content_provider": contentProvider,
+	}
+	return contentProviderTempl.Execute(f, templContext)
+}
+
+func renderDataSourceDoc(pluginSchema *plugin.Schema, dataSourceName string, dataSource *plugin.DataSource, fp string) error {
+	f, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var templContext = map[string]any{
+		"plugin":           pluginSchema,
+		"plugin_shortname": shortname(pluginSchema.Name),
+		"name":             dataSourceName,
+		"data_source":      dataSource,
+	}
+	return dataSourceTempl.Execute(f, templContext)
 }
 
 func shortname(name string) string {
@@ -88,16 +304,24 @@ func shortname(name string) string {
 	return name
 }
 
+func templateAttrTypeFunc(val hcldec.Spec) string {
+	switch v := val.(type) {
+	case *hcldec.AttrSpec:
+		return v.Type.FriendlyName()
+	default:
+		return "unknown"
+	}
+}
+
 func init() {
-	templ = template.Must(template.New("markdown").Funcs(template.FuncMap{
+	contentProviderTempl = template.Must(template.New("content-provider").Funcs(template.FuncMap{
+		"attrType": templateAttrTypeFunc,
+	}).Parse(contentProviderTemplValue))
+	dataSourceTempl = template.Must(template.New("data-source").Funcs(template.FuncMap{
+		"attrType": templateAttrTypeFunc,
+	}).Parse(dataSourceTemplValue))
+	pluginTempl = template.Must(template.New("plugin").Funcs(template.FuncMap{
+		"attrType": templateAttrTypeFunc,
 		"shortname": shortname,
-		"attrType": func(val hcldec.Spec) string {
-			switch v := val.(type) {
-			case *hcldec.AttrSpec:
-				return v.Type.FriendlyName()
-			default:
-				return "unknown"
-			}
-		},
-	}).Parse(markdownTempl))
+	}).Parse(pluginTemplValue))
 }
