91860b8e5e28d1c3cdbf310e31f832406e5201d7
Andrew Morozko <github-msgs-web@bitnotes.xyz>
2024-08-09 14:01:02 +0400

    Fetch tags Thanks to https://github.com/actions/checkout/pull/579#issuecomment-1615799806

--- codegen/gen_docs.sh
+++ codegen/gen_docs.sh
@@ -3,4 +3,4 @@
 set -e
 cd "$(dirname "$0")/.."
 
-go run ./tools/docgen --version $(git describe --tags --abbrev=0) --output ./docs/plugins
+go run ./tools/docgen --version $(git tag -l --sort=-creatordate | head -n1) --output ./docs/plugins
