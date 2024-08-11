4cfd508147af5cef0f9015e758e4363b094b8a69
Andrew Morozko <github-msgs-web@bitnotes.xyz>
2024-08-11 19:01:37 +0400

    Final script tweaks (#228)

--- codegen/gen_docs.sh
+++ codegen/gen_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Runs codegen tools
 set -e
-cd "$(dirname "$0")/.."
+cd "$(dirname "${BASH_SOURCE[0]:-$0}")/.."
 
 go run ./tools/docgen --version $(git tag -l --sort=-creatordate | head -n1) --output ./docs/plugins
