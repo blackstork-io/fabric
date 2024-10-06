7b3eb9ff1b0233ef04e48d0bbed770ef99eab91e
Anas Muhammed <anas.mu2022@gmail.com>
2024-10-06 15:05:16 +0400

    feat: tests

--- internal/crowdstrike/data_falcon_cspm_ioms.go
+++ internal/crowdstrike/data_falcon_cspm_ioms.go
@@ -6,10 +6,12 @@ import (
 	"github.com/crowdstrike/gofalcon/falcon"
 	"github.com/crowdstrike/gofalcon/falcon/client/cspm_registration"
 	"github.com/hashicorp/hcl/v2"
+	"github.com/zclconf/go-cty/cty"
 
 	"github.com/blackstork-io/fabric/pkg/diagnostics"
 	"github.com/blackstork-io/fabric/plugin"
 	"github.com/blackstork-io/fabric/plugin/dataspec"
+	"github.com/blackstork-io/fabric/plugin/dataspec/constraint"
 	"github.com/blackstork-io/fabric/plugin/plugindata"
 )
 
@@ -19,7 +21,14 @@ func makeFalconCspmIomsDataSource(loader ClientLoaderFn) *plugin.DataSource {
 		DataFunc: fetchFalconCspmIomsData(loader),
 		Config:   makeDataSourceConfig(),
 		Args: &dataspec.RootSpec{
-			Attrs: []*dataspec.AttrSpec{},
+			Attrs: []*dataspec.AttrSpec{
+				{
+					Name:        "size",
+					Type:        cty.Number,
+					Constraints: constraint.Integer | constraint.RequiredNonNull,
+					Doc:         "limit the number of queried items",
+				},
+			},
 		},
 	}
 }
@@ -34,7 +43,10 @@ func fetchFalconCspmIomsData(loader ClientLoaderFn) plugin.RetrieveDataFunc {
 				Detail:   err.Error(),
 			}}
 		}
+		size, _ := params.Args.GetAttrVal("size").AsBigFloat().Int64()
 		apiParams := cspm_registration.NewGetConfigurationDetectionsParams().WithDefaults()
+		apiParams.SetLimit(&size)
+		apiParams.Context = ctx
 		response, err := cli.CspmRegistration().GetConfigurationDetections(apiParams)
 		if err != nil {
 			return nil, diagnostics.Diag{{
