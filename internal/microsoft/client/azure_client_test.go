7f57f711f0923fadd8786b6abeb78417b1b21318
traut <sergey@polzunov.com>
2024-11-14 20:20:50 +0100

    Drop outdated comment

--- internal/microsoft/client/azure_client_test.go
+++ internal/microsoft/client/azure_client_test.go
@@ -47,44 +47,6 @@ func (s *ClientTestSuite) TestPrepare() {
 	client.QueryObjects(s.ctx, "/tmp", url.Values{}, 1)
 }
 
-// func (s *ClientTestSuite) TestListIncidents() {
-// 	client, srv := s.mock(func(w http.ResponseWriter, r *http.Request) {
-// 		expectPath := "/subscriptions/test_subscription_id/resourceGroups/test_resource_group/providers" +
-// 			"/Microsoft.OperationalInsights/workspaces/test_workspace/providers/Microsoft.SecurityInsights/incidents"
-// 		s.Equal(expectPath, r.URL.Path)
-// 		s.Equal(http.MethodGet, r.Method)
-// 		s.Equal("Bearer test_token", r.Header.Get("Authorization"))
-// 		s.Equal("10", r.URL.Query().Get("$top"))
-// 		s.Equal("test_filter", r.URL.Query().Get("$filter"))
-// 		s.Equal("test_order_by", r.URL.Query().Get("$orderby"))
-// 		w.Write([]byte(`{
-// 			"value": [
-// 				{
-// 					"any": "data"
-// 				}
-// 			]
-// 		}`))
-// 	}, "test_token")
-// 	defer srv.Close()
-// 	req := ListIncidentsReq{
-// 		SubscriptionID:    "test_subscription_id",
-// 		ResourceGroupName: "test_resource_group",
-// 		WorkspaceName:     "test_workspace",
-// 		Filter:            String("test_filter"),
-// 		OrderBy:           String("test_order_by"),
-// 		Top:               Int(10),
-// 	}
-// 	result, err := client.ListIncidents(s.ctx, &req)
-// 	s.NoError(err)
-// 	s.Equal(&ListIncidentsRes{
-// 		Value: []any{
-// 			map[string]any{
-// 				"any": "data",
-// 			},
-// 		},
-// 	}, result)
-// }
-
 func (s *ClientTestSuite) TestGetAllReportsError() {
 	client, srv := s.mock(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
