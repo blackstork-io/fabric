7f5d936accb0cedf7c5a4c9bd7a1c0c74434d3e7
dobarx <dominykas.barzdys+gh@gmail.com>
2025-02-19 22:21:30 +0200

    Upgrade mockery

--- gen.go
+++ gen.go
@@ -1,4 +1,4 @@
 package main
 
-//go:generate go run github.com/vektra/mockery/v2@v2.42.1
+//go:generate go run github.com/vektra/mockery/v2@v2.52.2
 //go:generate go run github.com/bufbuild/buf/cmd/buf@v1.32.2 generate
